f0191ad0dc91e0f56e05d424b54a0c6ade26e8a7
Кирилл Балезин <Kirill.Balezin@yandex.ru>
2023-02-21 20:06:37 +0300

    1.0

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
-
-from posts.models import Comment, Post
+from posts.models import Comment, Group, Post, Follow, User
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -13,11 +13,44 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
 
 
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+    author = serializers.StringRelatedField()
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_fields = ('author', 'post')
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
     )
 
     class Meta:
-        fields = '__all__'
-        model = Comment
+        fields = "__all__"
+        model = Follow
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+                message='Вы уже подписаны на этого пользователя.'
+            )
+        ]
+
+    def validate_following(self, value):
+        if self.context['request'].user == value:
+            raise serializers.ValidationError('Самоподписка запрещена.')
+        return value
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,18 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import PostViewSet, CommentViewSet, GrouptViewSet, FollowViewSet
+
+
+router_v1 = DefaultRouter()
+router_v1.register('v1/posts', PostViewSet)
+router_v1.register('v1/groups', GrouptViewSet)
+router_v1.register(r'^v1/posts/(?P<post_id>\d+)/comments', CommentViewSet,
+                   basename='comments')
+router_v1.register('v1/follow', FollowViewSet)
 
 urlpatterns = [
+    path('', include(router_v1.urls)),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
